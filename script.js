f5dff6fc1efdb37af4e33fdbd563bcdcbba333d5
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-02-20 18:58:56 +0600

    final || added

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// elements
+// selecting elements
 const check = document.querySelector('.check');
 const input = document.querySelector('.input-number');
-const guessingText = document.querySelector('.guessing-text');
-const correctAns = document.querySelector('.game-correct-ans');
+const guessText = document.querySelector('.guessing-text');
 const score = document.querySelector('.score');
 const highScore = document.querySelector('.high-score');
+const correctAns = document.querySelector('.game-correct-ans');
 const playAgain = document.querySelector('.play-again');
 
 // variables
@@ -14,25 +14,22 @@ let SECRET_NUMBER = Math.trunc(Math.random() * 20 + 1);
 let SCORE = 20;
 let HIGH_SCORE = 0;
 
-// display texts
-function displayGuessingText(text) {
-  guessingText.textContent = text;
+function displayGuessText(text) {
+  guessText.textContent = text;
 }
 
 // checking
 check.addEventListener('click', function () {
   const inputNumber = Number(input.value);
-
   // when there is no input
-  if (!inputNumber) displayGuessingText('Input a valid number!');
-  // when player wins
+  if (!inputNumber) displayGuessText('Input a valid number');
   else if (inputNumber === SECRET_NUMBER) {
-    displayGuessingText('Correct ans!');
+    displayGuessText('Correct Answer! You Win!!!🥳');
+    document.body.style.backgroundColor = 'teal';
     correctAns.textContent = SECRET_NUMBER;
     correctAns.style.backgroundColor = '#f1ebe6';
     correctAns.style.color = '#222222';
     correctAns.style.fontSize = '5.5rem';
-    document.body.style.backgroundColor = 'teal';
 
     if (SCORE > HIGH_SCORE) {
       HIGH_SCORE = SCORE;
@@ -43,24 +40,23 @@ check.addEventListener('click', function () {
   // when guess is wrong
   else if (inputNumber !== SECRET_NUMBER) {
     if (SCORE > 1) {
-      displayGuessingText(
-        inputNumber > SECRET_NUMBER ? 'Too high!' : 'Too low'
-      );
+      displayGuessText(inputNumber > SECRET_NUMBER ? 'Too High' : 'Too Low');
       SCORE--;
       score.textContent = SCORE;
     } else {
-      displayGuessingText('Game over!');
+      displayGuessText('Game Over! You loose!!!');
       score.textContent = 0;
     }
   }
 });
 
 // play again
+
 playAgain.addEventListener('click', function () {
   SCORE = 20;
   SECRET_NUMBER = Math.trunc(Math.random() * 20 + 1);
 
-  displayGuessingText('Start guessing...');
+  displayGuessText('Start Guessing...');
   document.body.style.backgroundColor = '#f1ebe6';
   correctAns.style.backgroundColor = '#000000b6';
   correctAns.textContent = '?';
